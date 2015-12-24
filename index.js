0a50412cdf0b58605eaa0356e05564c090436cf1
Martin Guillon <martin.guillon@akylas.fr>
2015-12-24 15:00:42 +0100

    a lot of fixes

--- index.js
+++ index.js
@@ -25,76 +25,89 @@ if (!stats.isDirectory()) {
     // Yes it is
 }
 // Automatically track and cleanup files at exit
-temp.track();
-
-var extension = path.extname(dsym);
-var appName = path.basename(dsym, extension);
-console.log('appName', appName);
-var uuidsString = execSync('dwarfdump --uuid "' + dsym + '"');
-console.log('uuidsString', uuidsString);
-var uuids = {};
-var re = /UUID:\s(.*)\s\((.*)\)/g;
-var match = re.exec(uuidsString),
-    group;
-while (match !== null) {
-    uuids[match[2]] = match[1].toLowerCase().replace(/-/g, '');
-    // matched text: match[0]
-    // match start: match.index
-    // capturing group n: match[n]
-    match = re.exec(uuidsString);
-}
-console.log('uuids', uuids);
-
+// temp.track();
 var dataLogs = [],
     resultLogs = [],
-    data;
-async.eachSeries(crashLogs, function(log, callback) {
-    stats = fs.lstatSync(log);
-    var cmd = path.join(__dirname, 'plcrashutil') + " convert --format=iphone \"" + log + "\"";
-    console.log(cmd);
-    cp.exec(cmd, function(err, stdout, stderr) {
-        if (err) {
-            console.log('stderr', stderr);
-        } else {
-            data = stdout;
-            match = /0x.*?([a-zA-Z0-9]*)\s+<(.*)>.*Alpi\ Maps/.exec(stdout);
-            if (match) {
-                var uuid = uuids[match[1]];
-                var toReplace = match[2];
-                console.log('uuid', uuid);
-                console.log('toReplace', toReplace);
-                data = data.replace(new RegExp(toReplace, 'g'), uuid);
-            }
-            dataLogs.push(data);
-        }
-        callback();
-    });
-}, function(err) {
-    // console.log(dataLogs);
-    async.eachSeries(dataLogs, function(dataLog, callback) {
-        temp.open('crash_log', function(err, info) {
-            if (!err) {
-                fs.write(info.fd, dataLog);
-                fs.close(info.fd, function(err) {
-                    var cmd = path.join(__dirname,
-                        'symbolicatecrash') + ' "' + info.path + '" "' + dsym + '"';
-                    console.log(cmd);
-                    cp.exec(cmd, function(
-                        err, stdout, stderr) {
-                        if (err) {
-                            console.log('stderr', stderr);
-                        } else {
-                            resultLogs.push(stdout);
-                        }
-                        callback();
-                    });
-                });
-            }
-        });
+    data,
+    extension = path.extname(dsym),
+    appName = path.basename(dsym, extension),
+    uuids = {},
+    re = /UUID:\s(.*)\s\((.*)\)/g,
+    group, cmd;
 
-    }, function(err2) {
-        _.each(resultLogs, function(log){
-            console.log(log);
+console.log('appName', appName);
+
+async.series([
+    function(callback) {
+        cmd = 'dwarfdump --uuid "' + dsym + '"';
+        console.log(cmd);
+        cp.exec(cmd, function(
+            err, stdout, stderr) {
+            if (err) {
+                throw new Error(err);
+            } else {
+                uuidsString = stdout;
+                console.log('uuidsString', uuidsString);
+                match = re.exec(uuidsString);
+                while (match !== null) {
+                    uuids[match[2]] = match[1].toLowerCase().replace(/-/g, '');
+                    match = re.exec(uuidsString);
+                }
+                console.log('uuids', uuids);
+            }
+            callback();
+        });
+    },
+], function(err, result) {
+    async.eachSeries(crashLogs, function(log, callback) {
+        stats = fs.lstatSync(log);
+        var cmd = path.join(__dirname, 'plcrashutil') + " convert --format=iphone \"" + log + "\"";
+        console.log(cmd);
+        cp.exec(cmd, function(err, stdout, stderr) {
+            if (err) {
+                console.log('stderr', stderr);
+            } else {
+                data = stdout;
+                match = /0x.*?([a-zA-Z0-9]*)\s+<(.*)>.*Alpi\ Maps/.exec(stdout);
+                if (match) {
+                    var uuid = uuids[match[1]];
+                    var toReplace = match[2];
+                    console.log('uuid', uuid);
+                    console.log('toReplace', toReplace);
+                    data = data.replace(new RegExp(toReplace, 'g'), uuid);
+                }
+                dataLogs.push(data);
+            }
+            callback();
+        });
+    }, function(err) {
+        // console.log(dataLogs);
+        async.eachSeries(dataLogs, function(dataLog, callback) {
+            temp.open('crash_log', function(err, info) {
+                if (!err) {
+                    fs.write(info.fd, dataLog);
+                    fs.close(info.fd, function(err) {
+                        var cmd = path.join(__dirname,
+                                'symbolicatecrash') + ' -v "' + info.path + '" "' +
+                            dsym + '"';
+                        console.log(cmd);
+                        cp.exec(cmd, function(
+                            err, stdout, stderr) {
+                            if (err) {
+                                console.log('stderr', stderr);
+                            } else {
+                                resultLogs.push(stdout);
+                            }
+                            callback();
+                        });
+                    });
+                }
+            });
+
+        }, function(err2) {
+            _.each(resultLogs, function(log) {
+                console.log(log);
+            });
         });
     });
 });
